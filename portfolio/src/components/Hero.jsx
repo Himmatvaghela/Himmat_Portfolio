e9461ff1f39aac6265c71f3c86d78707e175c57c
Himmat vaghela <himmatvaghela882@gmail.com>
2023-10-19 11:53:21 +0530

    first commit

---
+++ portfolio/src/components/Hero.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { styles } from '../styles'
+import {ComputersCanvas} from './canvas'
+import { MotionValue, motion } from 'framer-motion'
+
+const Hero = () => {
+  return (
+    <section className='relative w-full h-screen mx-auto'>
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='rounded-full w-5 h-5 bg-[#915eff]'/>
+          <div className='w-1 sm:h-80 h-40 violet-gradient'/>
+        </div>
+
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>Hi I&apos;m  
+            <span className='text-[#915eff]'> Himmat</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt2 text-white-100`}>I Am A Professional Frontend Developer. <br className='sm:flex hidden'/>Have Mastery In React & Angular.</p>
+        </div>
+
+      </div>
+        <ComputersCanvas/>
+
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-center p-2'>
+            <motion.dev 
+              animate={{
+                y:[0,24,0]
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType:'loop'
+              }}
+               className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
---
+++ portfolio/src/constants/index.js
@@ -0,0 +1,243 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    jobit,
+    tripguide,
+    threejs,
+    angular,
+    socially,
+    weatherApp,
+    typingspeed
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "Frontend Developer",
+      icon: mobile,
+    },
+    {
+      title: "React Developer",
+      icon: backend,
+    },
+    {
+      title: "Angular Developer",
+      icon: creator,
+    },
+    {
+      title:'MERN Developer',
+      icon: creator,
+    },
+    {
+      title:'MEAN Developer',
+      icon: backend,
+    },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name:'Angular JS',
+      icon:angular
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "Redux Toolkit",
+      icon: redux,
+    },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    {
+      name: "Three JS",
+      icon: threejs,
+    },
+    {
+      name: "git",
+      icon: git,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Angular JS Developer",
+      company_name: "United Web Enhencers",
+      icon: starbucks,
+      iconBg: "#383E56",
+      date: "Fabruary 2023 - Auguest 2021",
+      points: [
+        "Developing and maintaining web applications using Angular Js and other related technologies.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Made hotel booking platform with Angular Js and Typescript.","Implemented user-friendly hotel booking and advanced filtering for price, property, and type.","Utilized Angular, Material-UI, HTML, CSS, and JavaScript.","Role: Frontend Developer"
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "Sara Lee",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Socially",
+      description:
+        "Web-based social media platform that allows users to make strange Friends ,Upload Post it can be video ,image,audio user can like, comment on post.User can add and remove friend, last but not least fully responsive website.",
+      tags: [
+        {
+          name: "React",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "Express",
+          color: "pink-text-gradient",
+        },
+        {
+          name: "Node js",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Redux toolkit",
+          color: "green-text-gradient",
+        },
+      ],
+      image: socially,
+      view_website_link:'https://socially-b6do.onrender.com/',
+      source_code_link: "https://github.com/Himmatvaghela/Mern-Socially_App.git",
+    },
+    {
+      name: "Weather App",
+      description:
+        "A comprehensive Weather checking platform that allows users to check weather of any city by it city name and you will see cool icon according to weather.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "WeatherApi",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: weatherApp,
+      view_website_link:'https://himmatvaghela.github.io/WeatherApp/',
+      source_code_link: "https://github.com/Himmatvaghela/WeatherApp.git",
+    },
+    {
+      name: "Typing App",
+      description:
+        "A comprehensive Typing Speed checking App that allows users to check their typing speed and how many mistake has been made and every time a new paragraph come to type.",
+      tags: [
+        {
+          name: "Javascript",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "Html",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: typingspeed,
+      view_website_link:'https://himmatvaghela.github.io/Typing-website/',
+      source_code_link: "https://github.com/Himmatvaghela/Typing-website.git",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
